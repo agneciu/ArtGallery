b8788af09c4c6936bf99bb33a044148316f25512
agneciu <agneciuksyte@gmail.com>
2023-03-19 18:13:35 +0200

    working on artwork page display

--- art-gallery-app/src/components/PictureCard/PictureCard.jsx
+++ art-gallery-app/src/components/PictureCard/PictureCard.jsx
@@ -1,7 +1,5 @@
 import styles from "./PictureCard.module.css";
 import React from "react";
-import { Link } from "react-router-dom";
-
 
 const PictureCard = ({src , alt}) => {
 
--- art-gallery-app/src/pages/ArtworkPage/ArtworkPage.jsx
+++ art-gallery-app/src/pages/ArtworkPage/ArtworkPage.jsx
@@ -1,8 +1,31 @@
+import React, { useState, useEffect } from 'react';
 
 const ArtworkPage = () => {
-    return <div>
-        <p> Artwork page</p>
-    </div>
+   const [loading, setLoading] = useState(true);
+
+
+   
+   
+   return <div>
+       <img/>
+       <h2>title</h2>
+       <p>date</p>
+       <p>category</p>
+       <p>medium</p>
+       <select >
+          <option value="inches">Inches</option>
+          <option value="centimeters">Centimeters</option>
+        </select>
+        <p>dimensions</p>
+        <p>author</p>
+        <p>birthday</p>
+        <p>nationality</p>
+
+        <a>Similar artworks</a>
+
+
+
+        </div>
 }
 
 export default ArtworkPage
