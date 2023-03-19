b9ddd9423727d5b8f1be73c3f2328032f4fdb95c
agneciu <agneciuksyte@gmail.com>
2023-03-19 17:21:57 +0200

    redirect to "/artwork"

--- art-gallery-app/src/components/PictureCard/PictureCard.jsx
+++ art-gallery-app/src/components/PictureCard/PictureCard.jsx
@@ -1,11 +1,19 @@
 import styles from "./PictureCard.module.css";
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const PictureCard = ({src , alt}) => {
+
+  
+const redirectToArtwork = () => {
+  window.location.href = '/artwork'
+}
+
  return (
     <div>
   <div className={styles.root}>
-    <img className={styles.image} src={src} alt={alt} />
+    <img className={styles.image} src={src} alt={alt} onClick={redirectToArtwork} />
   </div>
   <p className={styles.author}>{alt}</p>
   </div>
